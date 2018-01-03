144105bbead54d334ba6db812ee2f9f54ea55160
Vietanhtvt <vietanhtvt@gmail.com>
2018-01-03 11:42:55 +0700

    second commit

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,15 @@
+public class Helloworld {
+    public static void main(String [] args){
+        int myInt = 7;
+        String text = "Helloworld";
+        String blank =" ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+        System.out.println(greeting);
+        System.out.println("Hello" + " " + "Bob");
+        System.out.println("My interger is: " + myInt);
+        double myDouble = 7.8;
+        System.out.printf("My double is : " + myDouble +".");
+    }
+}
+
